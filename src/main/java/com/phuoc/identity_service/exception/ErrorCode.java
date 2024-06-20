a04c6a5ef5a534776aac60ea57ff8b257863ac68
Nguyen Duc Phuoc <nguyenducphuoc0802@gmail.com>
2024-06-20 23:01:40 +0700

    add handle áºexception

--- src/main/java/com/phuoc/identity_service/exception/ErrorCode.java
+++ src/main/java/com/phuoc/identity_service/exception/ErrorCode.java
@@ -1,11 +1,15 @@
 package com.phuoc.identity_service.exception;
 
 public enum ErrorCode {
-    USER_EXISTED(1001, "User already existed"),
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),;
+    USER_EXISTED(1002, "User already existed"),
+    INVALID_KEY(1001, "Invalid message key"),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
+    PASSWORD_INVALID(1004, "Password must be at least 8 characters"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
+    ;
 
-    private int code;
-    private String message;
+    private final int code;
+    private final String message;
 
     ErrorCode (int code, String message) {
         this.code = code;
--- src/main/java/com/phuoc/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phuoc/identity_service/exception/GlobalExceptionHandler.java
@@ -20,8 +20,22 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<String> handlingMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body(ex.getBindingResult().getFieldError().getDefaultMessage());
+    ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        String enumKey = ex.getFieldError().getDefaultMessage();
+        ErrorCode errorCode;
+
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+        } catch (IllegalArgumentException e) {
+            errorCode = ErrorCode.INVALID_KEY;
+        }
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(AppException.class)
